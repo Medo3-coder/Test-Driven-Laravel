d206937195d454e78ea6b33a34b563cd6d4a3121
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2021-08-14 15:31:30 +0200

    add new path to book resource

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     use HasFactory;
    protected $guarded =[];
+
+   //make a path to redirect
+   public function path()
+   {
+
+       return '/books/' . $this->id ;   // books/1-enders-game
+   }
 
 }
